4d7878877fa7d84c4a37efc19f1d3734372d5c28
ZaminaNovruzova <novruzovazamina.mobile@gmail.com>
2025-08-14 22:37:46 +0400

    testimonials finished slider and icons not added

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// const swiper=new Swiper(".mySwiper",{
+//     slidesPerView:1,
+//     spaceBetween:16,
+// })
+
+// const swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 16,
+//   loop: true,
+//   autoplay: { delay: 3000, disableOnInteraction: false },
+//   pagination: { el: ".swiper-pagination", clickable: true },
+//   navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+//   breakpoints: {
+//     768: { slidesPerView: 2, spaceBetween: 20 },
+//     1100: { slidesPerView: 3, spaceBetween: 24 },
+//   },
+// });
